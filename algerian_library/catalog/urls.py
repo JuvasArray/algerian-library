3e19fe23799198b0e0218148a692bcdd5df812b7
juvas array <achouraithamiche40@gmail.com>
2018-09-20 18:10:31 +0100

    Changes

--- algerian_library/catalog/urls.py
+++ algerian_library/catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        path('book/', views.AuthorListView.as_view(), name='books'),
+        path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
--- algerian_library/catalog/views.py
+++ algerian_library/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Author, Book, Genre, Language, Publisher
+from django.views.generic import ListView
 
 def index(request):
     number_of_books = Book.objects.all().count()
@@ -10,3 +11,10 @@ def index(request):
             'number_of_authors': number_of_authors,
             }
     return render(request, 'catalog/index.html', context)
+
+class BookListView(ListView):
+    model = Book
+
+class AuthorListView(ListView):
+    model = Author
+
